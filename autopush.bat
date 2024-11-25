44067aa78d0106183aadb0f1af4463a186d04b51
eric <exqinhongwei@outlook.com>
2024-11-25 20:19:37 +0800

    auto push

--- autopush.bat
+++ autopush.bat
@@ -3,22 +3,22 @@ REM automate the git push process
 echo Navigate to project directory
 cd /d "C:\Users\exqin\Desktop\web"
 
-echo
+echo ..........
 
 echo Add all changes
 git add .
 
-echo
+echo ..........
 
 echo Commit changes, skip if no changes
 git commit -m "auto push" || echo No changes to commit.
 
-echo
+echo ..........
 
 echo Push to remote repository
 git push
 
-echo
+echo ..........
 
 echo Complete the task
 timeout /t 2

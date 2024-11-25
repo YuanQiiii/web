03b440b73ed7cf4474de69d4843f31651f1300a1
eric <exqinhongwei@outlook.com>
2024-11-25 21:27:38 +0800

    auto push

--- docs/.vitepress/navItems.js
+++ docs/.vitepress/navItems.js
@@ -1,124 +1,8 @@
 // 此文件由 generate-nav.js 自动生成，请勿手动修改
 export default [
   {
-    "text": "归档",
-    "items": [
-      {
-        "text": "人工智能",
-        "items": [
-          {
-            "text": "aibasisNote",
-            "link": "/归档/人工智能/aibasisNote"
-          },
-          {
-            "text": "aiNote",
-            "link": "/归档/人工智能/aiNote"
-          },
-          {
-            "text": "Q&A",
-            "link": "/归档/人工智能/Q&A"
-          }
-        ]
-      },
-      {
-        "text": "其他",
-        "items": [
-          {
-            "text": "debugger",
-            "link": "/归档/其他/debugger"
-          }
-        ]
-      },
-      {
-        "text": "数据结构与算法",
-        "items": [
-          {
-            "text": "algorithmNote",
-            "link": "/归档/数据结构与算法/algorithmNote"
-          },
-          {
-            "text": "algorithm_note",
-            "link": "/归档/数据结构与算法/algorithm_note"
-          },
-          {
-            "text": "codeNote",
-            "link": "/归档/数据结构与算法/codeNote"
-          },
-          {
-            "text": "dp_code",
-            "link": "/归档/数据结构与算法/dp_code"
-          },
-          {
-            "text": "exercise",
-            "link": "/归档/数据结构与算法/exercise"
-          },
-          {
-            "text": "problem_solution",
-            "link": "/归档/数据结构与算法/problem_solution"
-          },
-          {
-            "text": "recursion_code",
-            "link": "/归档/数据结构与算法/recursion_code"
-          }
-        ]
-      },
-      {
-        "text": "普通心理学",
-        "items": [
-          {
-            "text": "general_psychology",
-            "link": "/归档/普通心理学/general_psychology"
-          }
-        ]
-      },
-      {
-        "text": "程序设计",
-        "items": [
-          {
-            "text": "programNote",
-            "link": "/归档/程序设计/programNote"
-          }
-        ]
-      },
-      {
-        "text": "语言",
-        "items": [
-          {
-            "text": "cplusplusNote",
-            "link": "/归档/语言/cplusplusNote"
-          },
-          {
-            "text": "latexNote",
-            "link": "/归档/语言/latexNote"
-          },
-          {
-            "text": "psychology_statistics",
-            "link": "/归档/语言/psychology_statistics"
-          },
-          {
-            "text": "pythonNote",
-            "link": "/归档/语言/pythonNote"
-          },
-          {
-            "text": "rNote",
-            "link": "/归档/语言/rNote"
-          },
-          {
-            "text": "todo_list",
-            "link": "/归档/语言/todo_list"
-          }
-        ]
-      },
-      {
-        "text": "高性能计算",
-        "items": [
-          {
-            "text": "hpcNote",
-            "link": "/归档/高性能计算/hpcNote"
-          }
-        ]
-      }
-    ]
+    "text": "friends",
+    "link": "/friends"
   },
   {
     "text": "笔记",
@@ -126,6 +10,10 @@ export default [
       {
         "text": "书签",
         "items": [
+          {
+            "text": "注意",
+            "link": "/笔记/书签/注意"
+          },
           {
             "text": "电子书",
             "link": "/笔记/书签/电子书"
---
+++ docs/friends.md
@@ -0,0 +1,30 @@
+---
+layout: page
+---
+<script setup>
+import {
+  VPTeamPage,
+  VPTeamPageTitle,
+  VPTeamMembers
+} from 'vitepress/theme'
+
+const members = [
+  {
+    avatar: 'https://pku-cs-cjw.top/image/头像.jpg',
+    name: 'c+v',
+    title: 'Friend',
+    links: [
+      { icon: ⚡️
+      , link: 'https://pku-cs-cjw.top/' }
+    ]
+  }
+]
+
+</script>
+
+<VPTeamPage>
+  <VPTeamPageTitle>
+    Friends
+  </VPTeamPageTitle>
+  <VPTeamMembers :members="members" />
+</VPTeamPage>

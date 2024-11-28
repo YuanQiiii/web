ce5329fb6c5dc5ba86716f0f55c83dc3d7431955
eric <exqinhongwei@outlook.com>
2024-11-28 15:44:28 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,6 @@
 
 import navItems from './navItems' // 导入生成的导航菜单项
 import sidebarItems from './sidebarItems' // 导入生成的侧边栏
-import socialLinks from './socialLinks' // 导入社交链接
 
 export default {
   lastUpdated: true, // 使用 git 提交获取时间戳，使默认主题能够显示页面的上次更新时间
@@ -28,9 +27,6 @@ export default {
     ],
     sidebar: [
       ...sidebarItems // 合并生成的侧边栏
-    ],
-    socialLinks: [
-      ...socialLinks // 合并社交链接
     ]
   }
 }
